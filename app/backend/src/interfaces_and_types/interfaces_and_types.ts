66283ccd5e33f53430ac0d9d7234856c8ff98e0d
Vinicius <vini_bodra@hotmail.com>
2022-05-14 12:21:49 -0300

    criando interfaces e tipos iniciais

---
+++ app/backend/src/interfaces_and_types/interfaces_and_types.ts
@@ -0,0 +1,47 @@
+export type User = {
+  id: number,
+  username: string,
+  role: string,
+  email: string,
+  token?: string
+};
+
+export type Team = {
+  id: number,
+  teamName: string,
+};
+
+export interface IMatchGoals {
+  homeTeamGoals: number,
+  awayTeamGoals: number,
+}
+
+export interface IMatchesRequest extends IMatchGoals {
+  id?: number,
+  homeTeam: number,
+  awayTeam: number,
+  inProgress: boolean,
+}
+
+export interface IMatches extends IMatchGoals {
+  id: number,
+  homeTeam: number,
+  awayTeam: number,
+  inProgress: boolean,
+  teamHome: { teamName: string },
+  teamAway: { teamName: string },
+}
+
+export type LeaderBoard = {
+  rank?: number,
+  name: string,
+  totalPoints: number,
+  totalGames: number,
+  totalVictories: number,
+  totalDraws: number,
+  totalLosses: number,
+  goalsFavor: number,
+  goalsOwn: number,
+  goalsBalance: number,
+  efficiency: number
+};
