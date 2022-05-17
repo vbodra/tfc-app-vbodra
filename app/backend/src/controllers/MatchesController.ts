a959e9f740285420368e53a7245470fc64fa2894
Vinicius <vini_bodra@hotmail.com>
2022-05-17 20:30:33 -0300

    Implementando query param

--- app/backend/src/controllers/MatchesController.ts
+++ app/backend/src/controllers/MatchesController.ts
@@ -1,16 +1,27 @@
 import { NextFunction, Response, Request } from 'express';
+import { IMatches } from '../interfaces_and_types/types';
 import { IMatchesService } from '../interfaces_and_types/interfaces';
 
 export default class MatchesController {
   private _matchesServices: IMatchesService;
 
+  private _inProgress: boolean;
+
   constructor(matchesServices: IMatchesService) {
     this._matchesServices = matchesServices;
   }
 
-  public async getAll(_req: Request, res: Response, _next: NextFunction): Promise<void> {
-    const matches = await this._matchesServices.getAll();
+  public async getAll(req: Request, res: Response, _next: NextFunction): Promise<Response> {
+    const { inProgress } = req.query;
 
-    res.status(200).json(matches);
+    const matches = await this._matchesServices.getAll() as IMatches[];
+    if (!inProgress) {
+      return res.status(200).json(matches);
+    }
+    this._inProgress = await JSON.parse(inProgress as string);
+
+    const filteredMatches = matches?.filter((match) => match.inProgress === this._inProgress);
+
+    return res.status(200).json(filteredMatches);
   }
 }
